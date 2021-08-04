86a187107c972b94204a6b56ddac9a8f5716d8f5
Andreas Rossa <andreas@rossamail.de>
2021-08-05 00:50:18 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,35 @@
+zipit
+=====
+
+Takes mod files and zips them in a specific structure
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/zipit.svg)](https://npmjs.org/package/zipit)
+[![Downloads/week](https://img.shields.io/npm/dw/zipit.svg)](https://npmjs.org/package/zipit)
+[![License](https://img.shields.io/npm/l/zipit.svg)](https://github.com/andreasrossa/zipit/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g zipit
+$ zipit (-v|--version|version)
+zipit/0.0.0 darwin-arm64 node-v16.5.0
+$ zipit --help [COMMAND]
+USAGE
+  $ zipit [output folder] [-p <regex>]
+...
+```
+The specified capture group has to include 2 capture groups - one for the name and one for the version.
+
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+
+<!-- commandsstop -->
+
+# Todo 
+- let the user specify the folder structure via yaml file
---
+++ src/index.ts
@@ -0,0 +1,129 @@
+import { Command, flags } from '@oclif/command';
+import * as archiver from "archiver";
+import * as fs from "fs-extra";
+
+const FILE_EXTENSION = ".zip"
+const nameRegex = /([a-z-]+?)-([0-9]+\.[0-9]+\.[0-9]+)/g
+
+declare global {
+  interface String {
+    goodLastIndexOf: (i: string) => number | undefined
+  }
+}
+
+String.prototype.goodLastIndexOf = function(i: string) {
+  const a = String(this)
+  const result = a.lastIndexOf(i)
+  return result === -1 ? undefined : result
+}
+
+class Zipit extends Command {
+  static description = 'zips shit up - f a s t'
+
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({char: 'v'}),
+    help: flags.help({char: 'h'}),
+
+    input: flags.string({char: 'i', description: 'input folder'}),
+    pattern: flags.string({char: 'p', description: 'regex pattern to match source files'})
+  }
+
+  static args = [
+    {
+      name: 'output',
+      required: true,
+      description: "output folder",
+      default: "./zippedit"
+    }
+  ];
+
+  async run() {
+    const {args: { output = "./zippedit" }, flags: { input = ".", pattern }} = this.parse(Zipit)
+
+    const tempFolderName = ".zipittemp"
+    const tempFolder = `${input}/${tempFolderName}`;
+    
+    if(!fs.existsSync(tempFolder)) {
+      fs.mkdirSync(tempFolder)
+    }
+
+    if(!fs.existsSync(output)) {
+      fs.mkdirSync(output)
+    }
+
+    console.log("Collecting files...")
+    const files = fs.readdirSync(input, {withFileTypes: true}).filter(it => it.isFile()).map(it => it.name)
+
+    for(const file of files) {
+      handleFile(file, tempFolder, input, output, pattern)
+    }
+
+  }
+}
+
+const handleFile = (file: string, tempFolder: string, input: string, output: string, pattern?: string) => {
+  const fileName = (() => {
+    if(!file.includes(".")) return file
+
+    let r  = file
+    if(file.startsWith(".")) {
+      r = file.slice(1, undefined)
+    }
+
+    return r.slice(0, r.goodLastIndexOf(".")) + FILE_EXTENSION
+  })()
+  
+  let regex = nameRegex
+
+  if(pattern !== undefined) {
+    try {
+      regex = RegExp(pattern, 'g')
+    } catch(err) {
+      if(err instanceof SyntaxError) {
+        throw Error(`Provided pattern is invalid - ${err.message}`)
+      } 
+
+      throw Error(`Failed while instantiating pattern regex: ${err}`)
+    }
+  }
+  
+  const regexResult = regex.exec(fileName)
+
+  if(regexResult === null) {
+    console.warn(`File ${file} does not match the specified pattern: ${regex.source} - Skipping...`)
+    return
+  } else if(regexResult.length !== 3) {
+    throw Error("File matched the regex, but the regex contains more or less than 2 capture groups.")
+  }
+
+  const modFolder = fs.mkdtempSync(`${tempFolder}/`)
+  fs.mkdirSync(`${modFolder}/mods`)
+  fs.mkdirSync(`${modFolder}/config`)
+  fs.copySync(`${input}/${file}`, `${modFolder}/mods/${file}`)
+
+  const zipFilePath = `${output}/${fileName}`
+
+  const zipfile = fs.createWriteStream(zipFilePath)
+  const archive = archiver('zip', {})
+
+  archive.on('warning', function(err) {
+    if (err.code === 'ENOENT') {
+      console.warn(err.message)
+    } else {
+      throw err
+    }
+  });
+
+  archive.on('error', function(err) {
+    throw err;
+  });
+
+  archive.pipe(zipfile);
+
+  archive.directory(modFolder, false)
+  
+  archive.finalize()
+}
+
+export = Zipit
