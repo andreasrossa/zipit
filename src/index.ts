41ecc75deff81309c56980d3debd6bea57d7666f
Andreas Rossa <andreas@rossamail.de>
2021-08-05 18:55:34 +0200

    Fix it all, baby

--- src/index.ts
+++ src/index.ts
@@ -1,147 +1,186 @@
-import { Command, flags } from '@oclif/command';
+import { Command, flags } from "@oclif/command";
 import * as archiver from "archiver";
 import * as fs from "fs";
 import * as rimraf from "rimraf";
 
-const FILE_EXTENSION = ".zip"
-const nameRegex = /([a-z-]+?)-([0-9]+\.[0-9]+\.[0-9]+)/g
+const FILE_EXTENSION = ".zip";
+const nameRegex = /([a-z-]+?)-([0-9]+\.[0-9]+\.[0-9]+)/g;
 
 declare global {
   interface String {
-    goodLastIndexOf: (i: string) => number | undefined
+    goodLastIndexOf: (i: string) => number | undefined;
   }
 }
 
-String.prototype.goodLastIndexOf = function(i: string) {
-  const a = String(this)
-  const result = a.lastIndexOf(i)
-  return result === -1 ? undefined : result
-}
+String.prototype.goodLastIndexOf = function (i: string) {
+  const a = String(this);
+  const result = a.lastIndexOf(i);
+  return result === -1 ? undefined : result;
+};
 
 class Zipit extends Command {
-  static description = 'zips shit up - f a s t'
+  static description = "zips shit up - f a s t";
 
   static flags = {
     // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
 
-    input: flags.string({char: 'i', description: 'input folder'}),
-    pattern: flags.string({char: 'p', description: 'regex pattern to match source files'}),
-    clear: flags.boolean({char: 'c', description: 'deletes the temporary .zipittemp folder on completion'})
-  }
+    input: flags.string({ char: "i", description: "input folder" }),
+    pattern: flags.string({
+      char: "p",
+      description: "regex pattern to match source files",
+    }),
+    clear: flags.boolean({
+      char: "c",
+      description: "deletes the temporary .zipittemp folder on completion",
+    }),
+  };
 
   static args = [
     {
-      name: 'output',
+      name: "output",
       required: true,
       description: "output folder",
-      default: "./zippedit"
-    }
+      default: "./zippedit",
+    },
   ];
 
   async run() {
-    const {args: { output = "./zippedit" }, flags: { input = ".", pattern, clear }} = this.parse(Zipit)
+    const {
+      args: { output = "./zippedit" },
+      flags: { input = ".", pattern, clear },
+    } = this.parse(Zipit);
 
-    const tempFolderName = ".zipittemp"
+    const tempFolderName = ".zipittemp";
     const tempFolder = `${input}/${tempFolderName}`;
-    
-    if(!fs.existsSync(tempFolder)) {
-      fs.mkdirSync(tempFolder)
+
+    if (!fs.existsSync(tempFolder)) {
+      fs.mkdirSync(tempFolder);
     }
 
-    if(!fs.existsSync(output)) {
-      fs.mkdirSync(output)
+    if (!fs.existsSync(output)) {
+      fs.mkdirSync(output);
     }
 
-    console.log("Collecting files...")
-    const files = fs.readdirSync(input, {withFileTypes: true}).filter(it => it.isFile()).map(it => it.name)
+    console.log("Collecting files...");
+    const files = fs
+      .readdirSync(input, { withFileTypes: true })
+      .filter((it) => it.isFile())
+      .map((it) => it.name);
 
-    for(const file of files) {
-      handleFile(file, tempFolder, input, output, pattern)
+    let processedFiles = 0;
+
+    for (const file of files) {
+      let r = null;
+      try {
+        r = await handleFile(file, tempFolder, input, output, pattern);
+      } catch (err) {
+        console.warn(`Failed to handle file ${file}`);
+        continue;
+      }
+
+      if (r === true) {
+        processedFiles++;
+        console.log(`Processed '${file}' successfully!`)
+      }
     }
 
-
-    if(clear === true) {
-      rimraf(tempFolder, (err) => {
-        if(!!err) {
-          throw Error(`Failed to remove temp folder: ${err.message}`)
-        }
-      })
+    if (clear === true) {
+      rimraf.sync(tempFolder, {
+        lstat: fs.lstat,
+      });
     }
+
+    console.log(`\n\nFinished! Processed ${processedFiles} files successfully.`);
   }
 }
 
-const handleFile = (file: string, tempFolder: string, input: string, output: string, pattern?: string) => {
+const handleFile = async (
+  file: string,
+  tempFolder: string,
+  input: string,
+  output: string,
+  pattern?: string
+): Promise<boolean> => {
   const fileName = (() => {
-    if(!file.includes(".")) return file
+    if (!file.includes(".")) return file;
 
-    let r  = file
-    if(file.startsWith(".")) {
-      r = file.slice(1, undefined)
+    let r = file;
+    if (file.startsWith(".")) {
+      r = file.slice(1, undefined);
     }
 
-    return r.slice(0, r.goodLastIndexOf(".")) + FILE_EXTENSION
-  })()
-  
-  let regex = nameRegex
+    return r.slice(0, r.goodLastIndexOf(".")) + FILE_EXTENSION;
+  })();
 
-  if(pattern !== undefined) {
+  let regex = nameRegex;
+
+  if (pattern !== undefined) {
     try {
-      regex = RegExp(pattern, 'g')
-    } catch(err) {
-      if(err instanceof SyntaxError) {
-        throw Error(`Provided pattern is invalid - ${err.message}`)
-      } 
+      regex = RegExp(pattern, "g");
+    } catch (err) {
+      if (err instanceof SyntaxError) {
+        throw Error(`Provided pattern is invalid - ${err.message}`);
+      }
 
-      throw Error(`Failed while instantiating pattern regex: ${err}`)
+      throw Error(`Failed while instantiating pattern regex: ${err}`);
     }
   }
-  
-  const regexResult = regex.exec(fileName)
 
-  if(regexResult === null) {
-    console.warn(`File ${file} does not match the specified pattern: ${regex.source} - Skipping...`)
-    return
-  } else if(regexResult.length !== 3) {
-    throw Error("File matched the regex, but the regex contains more or less than 2 capture groups.")
+  let regexResult = null;
+
+  try {
+    regexResult = regex.exec(fileName);
+  } catch (err) {
+    console.error(`failed while executing regex on '${fileName}'`);
+  }
+
+  if (regexResult === null) {
+    console.warn(
+      `File ${file} does not match the specified pattern: ${regex.source} - Skipping...`
+    );
+    return false;
+  } else if (regexResult.length !== 3) {
+    throw Error(
+      "File matched the regex, but the regex contains more or less than 2 capture groups."
+    );
   }
 
   let modFolder;
   try {
-    modFolder = fs.mkdtempSync(`${tempFolder}/`)
-    fs.mkdirSync(`${modFolder}/mods`)
-    fs.mkdirSync(`${modFolder}/config`)
-    fs.copyFileSync(`${input}/${file}`, `${modFolder}/mods/${file}`)
+    modFolder = fs.mkdtempSync(`${tempFolder}/`);
+    fs.mkdirSync(`${modFolder}/mods`);
+    fs.mkdirSync(`${modFolder}/config`);
+    fs.copyFileSync(`${input}/${file}`, `${modFolder}/mods/${file}`);
   } catch (err) {
-    throw Error(`Failed while creating folder structure: ${(<Error>err).message}`)
+    throw Error(
+      `Failed while creating folder structure: ${(<Error>err).message}`
+    );
   }
 
-  const zipFilePath = `${output}/${fileName}`
+  const zipFilePath = `${output}/${fileName}`;
 
-  const zipfile = fs.createWriteStream(zipFilePath)
-  const archive = archiver('zip', {})
+  const zipfile = fs.createWriteStream(zipFilePath);
+  const archive = archiver("zip", {});
 
-  archive.on('warning', function(err) {
-    if (err.code === 'ENOENT') {
-      console.warn(err.message)
+  archive.on("warning", function (err) {
+    if (err.code === "ENOENT") {
+      console.warn(err.message);
     } else {
-      throw err
+      throw err;
     }
   });
 
-  archive.on('error', function(err) {
-    throw err;
+  archive.on("error", function (err) {
+    throw Error(`Failed while creating zip: ${err.message}`);
   });
 
-  try {
-    archive.pipe(zipfile)
-    archive.directory(modFolder, false)
-    archive.finalize()
-  } catch (err) {
-    console.error(`Failed during archiving for file '${file}'. Skipping...`)
-    return
-  }
-}
+  archive.pipe(zipfile);
+  archive.directory(modFolder, false);
+  await archive.finalize();
 
-export = Zipit
+  return true;
+};
+
+export = Zipit;
